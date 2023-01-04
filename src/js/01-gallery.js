efd3c290ec4d0204df912c69da35126f868275f8
alkatsalex <alkatsalex@ukr.net>
2023-01-05 00:34:00 +0200

    hw done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,78 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`)
+
+const marckup = galleryItems.map(({preview, original, description,}) => 
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src = "${preview}"
+      data-source= "${original}"
+      alt= "${description}"
+    />
+  </a>
+</div>`
+).join("")
+
+gallery.insertAdjacentHTML("beforeend", marckup)
+
+
+gallery.addEventListener(`click`, choosePhoto)
+
+function choosePhoto(e) {
+    if (!e.target.classList.contains(`gallery__image`)) {
+        return
+    } else {
+
+        e.preventDefault();
+        const instance = basicLightbox.create(
+            `<img src="${e.target.dataset.source}" alt="">`
+        )
+
+instance.show()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(basicLightbox);
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`)
+
+const marckup = galleryItems.map(({preview, original, alt ,description,}) => 
+    `<div class="gallery">
+    <a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="" title="Beautiful Image"/></a>
+</div>`
+).join("")
+
+gallery.insertAdjacentHTML("beforeend", marckup)
+
+gallery.addEventListener(`click`, choosePhoto)
+
+function choosePhoto(e) {
+    if (!e.target.classList.contains(`gallery__image`)) {
+        return
+    } else {
+
+        e.preventDefault();
+
+      var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
+      console.log(lightbox);
+    }
+}
